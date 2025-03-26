7c32314925ad04d5f7b5670cd48b11169dbd66dd
Umesh Khanal <116002445+umeshkhanal@users.noreply.github.com>
2025-03-26 15:38:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -639,4 +639,4 @@ def top_scorers(tournament_id):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
