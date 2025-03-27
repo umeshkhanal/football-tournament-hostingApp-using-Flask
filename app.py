1bb63dfa6a176a9f0a4b1c651f3f42a84495b0b1
Umesh Khanal <116002445+umeshkhanal@users.noreply.github.com>
2025-03-27 22:25:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ from sqlalchemy import desc, func
 
 app = Flask(__name__)
 app.secret_key = "your_secret_key_here"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tournament.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:119139@localhost:5432/tournament_db'
+
 db = SQLAlchemy(app)
 
 class Tournament(db.Model):
